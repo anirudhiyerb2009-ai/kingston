7500aa5ae61c6d8437152ee10999939003d21f39
Anirudh Iyer <anirudhiyer.b2009@gmail.com>
2025-08-05 08:56:08 +1000

    Add first workflow

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';  // Import BrowserRouter here
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+serviceWorkerRegistration.register();
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
